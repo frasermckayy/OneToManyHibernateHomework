877f2aa2b0011d12a4f73fdc6f0d8ddca012d853
Fraser McKay <frasermckay97@gmail.com>
2018-05-16 01:13:01 +0100

    created runner

--- src/main/java/folder/Runner.java
+++ src/main/java/folder/Runner.java
@@ -1,6 +1,23 @@
+import folder.db.DBFile;
+import folder.db.DBFolder;
+import folder.models.Folder;
+
+import java.io.File;
+
 public class Runner {
 
     public static void main(String[] args) {
+        Folder fold1 = new Folder("HR");
+        DBFolder.save(fold1);
+
+        Folder fold2 = new Folder("Sales");
+        DBFolder.save(fold2);
+
+        File file1 = new File("fraser's", ".txt", 56, fold1);
+        DBFile.save(file1);
+
+        File file2 = new File("tom's", ".rb", 98, fold2);
+        DBFile.save(file2);
 
     }
 }
